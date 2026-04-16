fc49e8f30075f9e2781ef6a35c439a66592bd3cb
vedanthanekar45 <vedanthanekar45@gmail.com>
2026-04-16 14:41:38 +0530

    chore: example

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ public class Program
     public static void Main(string[] args)
     {
         var service = new TaskService();
+        service.AddTask("Learn C#", "Work");
+        service.AddTask("Gym", "Personal");
 
+        var tasks = service.GetAllTasks();
+
+        foreach (var t in tasks)
+        {
+            Console.WriteLine($"{t.Id} - {t.Title} - {t.isCompleted}");
+        }
     }
 }
